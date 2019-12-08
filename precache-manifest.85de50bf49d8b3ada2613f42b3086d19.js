f25ec275b5963b9e4d72c2eecc88d2d3df402d25
umuttalipoglu94 <yazan.taleb@ogr.sakarya.edu.tr>
2019-12-08 14:03:36 +0300

    Updates

---
+++ precache-manifest.85de50bf49d8b3ada2613f42b3086d19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a56aab9b00d1f54f56e6216e23089bf",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "2ef7d8e19d6369877f9d",
+    "url": "/burger-builder/static/css/main.d4cfcd24.chunk.css"
+  },
+  {
+    "revision": "a4a2da64e41fcb3387a1",
+    "url": "/burger-builder/static/js/2.8d641d8c.chunk.js"
+  },
+  {
+    "revision": "2ef7d8e19d6369877f9d",
+    "url": "/burger-builder/static/js/main.ff074fd7.chunk.js"
+  },
+  {
+    "revision": "f26687a28f5dfa8916ba",
+    "url": "/burger-builder/static/js/runtime-main.a70e4314.js"
+  }
+]);
